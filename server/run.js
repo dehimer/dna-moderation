3f57dc6b5ed21f7e7f289bbd2602ab59c8875e39
Denis Novikov <den-vse@yandex.ru>
2019-09-26 16:07:23 +0300

    path to config

--- server/run.js
+++ server/run.js
@@ -8,7 +8,7 @@ const answersDb = new Datastore({ filename: 'answers', autoload: true });
 
 const express = require('express');
 
-const config  = require('./config');
+const config  = require('./config.js');
 
 const app = express();
 
